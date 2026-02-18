b23937e7025636747575edc7ee34ab44bd406eb6
SHAMSHAD7643 <shaim7643@gmail.com>
2026-02-18 14:55:24 +0530

    Added class0 project

---
+++ class0/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>i am shamshad ansari</h1>
+      <p>from cohort 2.0</p>
+    </div>
+  )
+}
+
+export default App
---
+++ class0/src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client'
+import app from './App'
+
+ const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(app())
