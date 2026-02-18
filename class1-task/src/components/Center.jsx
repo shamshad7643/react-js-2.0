8c1f1ee0408b22cbaba5abb0cefd651d0aac2a64
SHAMSHAD7643 <shaim7643@gmail.com>
2026-02-18 15:57:47 +0530

    Added class1-task

---
+++ class1-task/src/components/Center.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Center = () => {
+  return (
+    <div>
+        <h1 className='cen'>This is a center</h1>
+    </div>
+  )
+}
+
+export default Center
---
+++ class1-task/src/components/Section.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Center from './center'
+
+const Section = () => {
+  return (
+    <div className='sec'>
+        <Center />
+
+    </div>
+  )
+}
+
+export default Section
