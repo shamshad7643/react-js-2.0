0728a6b6f21ffbe0a49e6155f81038bec4b9e246
SHAMSHAD7643 <shaim7643@gmail.com>
2026-02-19 16:31:02 +0530

    added class2

---
+++ class2/src/App.jsx
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='bg-amber-600 h-25'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
